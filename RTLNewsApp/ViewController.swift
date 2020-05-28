d431dad61a1542ee2700bc70cc4b27267fd05b47
Abdorahman <Abdorahmanmahmoudd@gmail.com>
2020-05-28 16:59:27 +0200

    Initial Commit

---
+++ RTLNewsApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RTLNewsApp
+//
+//  Created by Abdorahman on 28/05/2020.
+//  Copyright © 2020 Abdorahman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
