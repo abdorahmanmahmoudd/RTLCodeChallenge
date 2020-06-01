3512d9490e060eccbc8119f7af3c177c79012d4c
Abdorahman <Abdorahmanmahmoudd@gmail.com>
2020-06-01 14:59:41 +0200

    push pods

---
+++ Pods/Target Support Files/Pods-RTLNewsApp/Pods-RTLNewsApp-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_RTLNewsApp : NSObject
+@end
+@implementation PodsDummy_Pods_RTLNewsApp
+@end
---
+++ Pods/Target Support Files/Pods-RTLNewsAppTests/Pods-RTLNewsAppTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_RTLNewsAppTests : NSObject
+@end
+@implementation PodsDummy_Pods_RTLNewsAppTests
+@end
